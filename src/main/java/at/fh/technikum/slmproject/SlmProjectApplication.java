759ccb640d7a52bb2164e657228d600bdf1d832a
David Tinhofer <da@w23.at>
2023-05-17 10:52:02 +0200

    inital commit second part

---
+++ src/main/java/at/fh/technikum/slmproject/SlmProjectApplication.java
@@ -0,0 +1,13 @@
+package at.fh.technikum.slmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fh/technikum/slmproject/SlmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fh.technikum.slmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
